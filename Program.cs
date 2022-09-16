e3884d513ce1450402c8b6d6fd3264cfc5a0a21f
Alex Cap <jamurmansk@yandex.ru>
2022-09-16 22:31:47 +0300

    The final project is done

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string[] FilterArray(string[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+
+    string[] filtredArr = new string[count];
+    count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            filtredArr[count] = array[i];
+            count++;
+        }
+    }
+
+    return filtredArr;
+} 
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+string[] arr = {"Hello", "97", "world", ":-)"};
+PrintArray(arr);
+string[] filtredArr = FilterArray(arr);
+Console.Write(" -> ");
+PrintArray(filtredArr);
